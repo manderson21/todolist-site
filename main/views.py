e33e23feb622646dba2cf4dcdd69c27658b78845
manderson <anderson.c.matt@gmail.com>
2021-06-03 15:42:48 -0500

    Added a second link in the webapp

--- main/views.py
+++ main/views.py
@@ -5,3 +5,7 @@ from django.http import HttpResponse
 # Create your views here.
 def index(response):
     return HttpResponse("<h1>Matt's Site!</h1>")
+
+
+def v1(response):
+    return HttpResponse("<h1>view 1!</h1>")
