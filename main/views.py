43a4629122b2f3f36f03b3d509d0d0f4dfc4675d
manderson <anderson.c.matt@gmail.com>
2021-06-04 00:22:42 -0500

    Create model for sqllite database and show it in the view

--- main/views.py
+++ main/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from main.models import ToDoList
 
 
 # Create your views here.
-def index(response):
-    return HttpResponse("<h1>Matt's Site!</h1>")
 
 
-def v1(response):
-    return HttpResponse("<h1>view 1!</h1>")
+def index(response, id):
+    to_do_list = ToDoList.objects.get(id=id)
+    item = to_do_list.item_set.get(id=1)
+    return HttpResponse("<h1>%s</h1></br><p>%s</p>" % (to_do_list.name, item.text))
