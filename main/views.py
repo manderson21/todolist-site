ed4e218291bd83a0604fef06185a88e354a2a516
manderson <anderson.c.matt@gmail.com>
2021-06-06 01:09:19 -0500

    Create first form with django (doesn't work yet)

--- main/views.py
+++ main/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 from main.models import ToDoList
+from main.forms import CreateNewList
 
 
 # Create your views here.
 
 
-def index(response, id):
-    to_do_list = ToDoList.objects.get(id=id)
-    return render(response, "main/list.html", {"to_do_list":to_do_list})
+def index(response, id_):
+    to_do_list = ToDoList.objects.get(id=id_)
+    return render(response, "main/list.html", {"to_do_list": to_do_list})
 
 
 def home(response):
     return render(response, "main/home.html", {})
+
+
+def create(response):
+    form = CreateNewList()
+    return render(response, "main/create.html", {"form": form})
