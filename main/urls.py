0a267ed39aa80ee859abcafb3ca6b43b459de297
manderson <anderson.c.matt@gmail.com>
2021-06-07 10:47:07 -0500

    Make form work with a POST request

--- main/urls.py
+++ main/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:id>', views.index, name="index"),
+    path('<int:id_>', views.index, name="index"),
     path('', views.home, name="home"),
     path("create/", views.create, name="create"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 from main.models import ToDoList
@@ -17,5 +18,15 @@ def home(response):
 
 
 def create(response):
+    if response.method == "POST":
+        form = CreateNewList(response.POST)
+
+        if form.is_valid():
+            n = form.cleaned_data["name"]
+            t = ToDoList(name=n)
+            t.save()
+            return HttpResponseRedirect("/%i" % t.id)
+    else:
+        form = CreateNewList()
     form = CreateNewList()
     return render(response, "main/create.html", {"form": form})
