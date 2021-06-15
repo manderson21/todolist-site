1c88e7b088f1f01c1a5b589a348e7fe2eba013b6
manderson <anderson.c.matt@gmail.com>
2021-06-15 00:50:10 -0500

    Set app/list/ to show the base class instead of an error

--- todo/urls.py
+++ todo/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from django.views.generic import RedirectView
+from django.views.generic import RedirectView, TemplateView
 
 from todo import views
 
@@ -8,7 +8,7 @@ urlpatterns = [
     path('app/', RedirectView.as_view(url="list/")),
 
     # TODO: change home view to be a list
-    path('app/list/', views.home, name="home"),
+    path('app/list/', TemplateView.as_view(template_name="todo/base.html")),
     path('app/list/<int:id_>', views.form_edit_todo, name="form_edit_to_do_list"),
 
     # TODO: Create today and later views/templates
